dc0150402c00614da3b58e09b55b150115a2d5fc
candidlpd <candidlpd>
2024-07-29 23:28:57 +0000

    interview 1 is completed

--- pyspark_coding_interview_1.py
+++ pyspark_coding_interview_1.py
@@ -44,6 +44,17 @@ print(sys.executable)
 
 # COMMAND ----------
 
+from IPython.display import Image, display
+
+# Specify the path to the uploaded image
+image_path = '/Workspace/Users/100707-anv-28@deccansoftstudents.onmicrosoft.com/pyspark-coding-interview/data/name_hobbies.png.png
+'
+
+# Display the image
+display(Image(filename=image_path))
+
+# COMMAND ----------
+
 # Define the data and columns
 data = [
     ('Alice', 'Badminton, Tennis'),
@@ -57,6 +68,19 @@ df = spark.createDataFrame(data, columns)
 
 # Display the DataFrame
 df.show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import split
+df_array = df.withColumn("Hobbies_array", split(df.Hobbies, ','))
+display(df_array.show())
+
+# COMMAND ----------
+
+from pyspark.sql.functions import explode
+
+df_flatten = df_array.withColumn("Hobbies_flatten", explode(df_array["Hobbies_array"])).drop("Hobbies_array", "Hobbies")
+display(df_flatten.show())
 
 # COMMAND ----------
 
